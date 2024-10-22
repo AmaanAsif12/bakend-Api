1f4dea58043a03849b4496e856c6063ef37b3738
Amaan Asif <amaanasif858gmail.com>
2024-10-22 04:25:29 -0700

    first commit

--- index.mjs
+++ index.mjs
@@ -4,9 +4,12 @@ import mongoose from './config/db.js';  // Import the mongoose instance
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send({ message: "Hello from the server !" })
+})
+
 app.listen(3001, () => {
     console.log('Server is running on port 3001');
-
     mongoose.connection.once('open', () => {
         console.log('db connected successfully!')
     });
